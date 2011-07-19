c43b3446b90e7bb5fdca8d594e0a1087dbd6d2ca
Anatoly Lapshin <technologymasta@gmail.com>
2011-07-20 02:15:33 +0400

    style up

--- public/javascripts/messages.js
+++ public/javascripts/messages.js
@@ -5,10 +5,21 @@ $(function() {
   var jade = require('jade');
   var messageTemplate = jade.compile($('#brickTemplate').text());
 
+  function updateWallWidth() {
+    var $wall = $('.Wall');
+    var $lastBrick = $wall.find('.Brick:last');
+
+    console.log($lastBrick.position().left);
+
+    $('.WallWrapper').css({ width: $lastBrick.position().left - $lastBrick.height() });
+  }
+
   client.subscribe(Globals.stream, function(message) {
 
     $('.Wall').prepend(messageTemplate.call(this, {brick: message}));
-
+    updateWallWidth();
   });
+
+  updateWallWidth();
 
 });
